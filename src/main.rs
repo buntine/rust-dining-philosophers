b065528a6c14f9e1073bd4b61650f76c672acaa1
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-24 22:07:07 +1000

    Added eat function

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,27 @@ struct Philosopher {
 }
 
 impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+      Philosopher {
+          name: name.to_string(),
+      }
+    }
+
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
+}
+
+fn main() {
+    let philosophers = vec![
+        Philosopher::new("Bunts"),
+        Philosopher::new("Mel"),
+        Philosopher::new("Grace"),
+        Philosopher::new("Rohan"),
+        Philosopher::new("Angelina")
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
